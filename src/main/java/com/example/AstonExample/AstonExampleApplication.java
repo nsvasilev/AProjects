d280c60ab47ac31d032c5777d774d1800c6812d6
Nikita Vasilyev <Nik.vasilev.20@inbox.ru>
2024-06-24 14:41:10 +0300

    new project

---
+++ src/main/java/com/example/AstonExample/AstonExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.AstonExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstonExampleApplication.class, args);
+	}
+
+}
